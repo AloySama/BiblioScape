823e34f478699b75390de9b4fdb7c3e4c32ea2cc
a.brengard <alois.brengard74@gmail.com>
2022-10-22 14:50:59 +0200

    [+] Added banner

--- biblioscape/src/App.vue
+++ biblioscape/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <banner-app/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -28,3 +29,9 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import BannerApp from "@/components/Banner";
+export default {
+  components: {BannerApp}
+}
+</script>
---
+++ biblioscape/src/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <section>
+    <div class="container">
+      <header>
+        <div class="left">
+          <img src="../assets/logo.png" alt="logo" width="50" height="50">
+        </div>
+        <div class="right">
+          <nav>
+            <router-link v-for="item in list" :key="item" :to="item.to">{{item.name}}</router-link>
+          </nav>
+        </div>
+      </header>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Banner-app",
+  data() {
+    return {
+      list: [
+        {
+          name: "Qui sommes nous ?",
+          to: "about"
+        },
+        {
+          name: "Accueil",
+          to: "/"
+        }]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+section .container {
+  max-width: 1100px;
+  margin: 0 auto;
+  padding-top: 10px;
+}
+
+section .container header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+section .container nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+section .container nav a {
+  margin-left: 10px;
+}
+
+section .container a:nth-child(2) {
+  border: 2px solid black;
+  padding: 10px 20px;
+  border-radius: 20px;
+}
+
+</style>
